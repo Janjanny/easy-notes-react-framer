0798c4dea940b0c5ef5265764d70e4f31646e894
Janjanny <janjanbeboso0912@gmail.com>
2024-07-23 00:58:00 +0800

    add Link

--- easy-notes/app/components/Navbar.jsx
+++ easy-notes/app/components/Navbar.jsx
@@ -7,11 +7,13 @@ const Navbar = () => {
     <>
         <div className="sticky top-0 z-[99] w-full bg-white-bg">
           <nav className=" w-10/12 mx-auto py-[1.5rem]  flex justify-between items-center">
-            <h1 className=" font-bold text-[1.8rem]">EasyNotes</h1>
+            <Link href={"/"}><h1 className=" font-bold text-[1.8rem]">EasyNotes</h1></Link>
 
-            <div className="buttons">
-              <button className="flex items-center gap-2 border border-black py-[8px] px-[18px] rounded-[2rem]">Add Note <FaPlus /> </button>
-              <button className="flex items-center gap-2 border border-black py-[8px] px-[18px] rounded-[2rem]">Light <FaPlus /> </button>
+            <div className="buttons flex gap-[1rem] ">
+              <Link href={"/addNote"} className="flex items-center gap-2 border border-black py-[8px] px-[18px] rounded-[2rem]"> Add Note <FaPlus /> </Link>
+              <button className="flex items-center gap-2 border border-black py-[8px] px-[18px] rounded-[2rem] w-max"> Light <FaRegSun /> </button>
+              
+              
             </div>
           </nav>
         </div>
