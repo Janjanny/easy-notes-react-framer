5d3868ae6b6ab0c6ecb570ab3fa09d14ca400f66
Janjanny <janjanbeboso0912@gmail.com>
2024-07-23 01:09:08 +0800

    install framer

--- easy-notes/app/components/Navbar.jsx
+++ easy-notes/app/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaPlus, FaRegSun } from "react-icons/fa6";
+import { motion } from "framer-motion"
 
 import Link from "next/link";
 const Navbar = () => {
@@ -7,10 +8,11 @@ const Navbar = () => {
     <>
         <div className="sticky top-0 z-[99] w-full bg-white-bg">
           <nav className=" w-10/12 mx-auto py-[1.5rem]  flex justify-between items-center">
-            <Link href={"/"}><h1 className=" font-bold text-[1.8rem]">EasyNotes</h1></Link>
+            <motion.div initial={{opacity: 0}} animate={{opacity: 1}}><Link href={"/"}><h1 className=" font-bold text-[1.8rem]">EasyNotes</h1></Link></motion.div>
 
             <div className="buttons flex gap-[1rem] ">
               <Link href={"/addNote"} className="flex items-center gap-2 border border-black py-[8px] px-[18px] rounded-[2rem]"> Add Note <FaPlus /> </Link>
+
               <button className="flex items-center gap-2 border border-black py-[8px] px-[18px] rounded-[2rem] w-max"> Light <FaRegSun /> </button>
               
               
